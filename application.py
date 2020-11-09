8b93eec0fa405a0e326a6085fdc534e542c821f1
jopijuco <jpintojcoelho@gmail.com>
2020-11-09 14:08:56 +0000

    configure app start up and basic index and login page

--- application.py
+++ application.py
@@ -1,16 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, request, session
+from flask_session import Session
+from tempfile import mkdtemp
 
+# from working directory
+from db import create_connection
+
+# Configure application
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html")
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOADED"] = True
 
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
 
-@app.route("/register")
-def register():
-    return "TODO"
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
-@app.route("/login")
-def login():
-    return render_template("login.html")
+# Configure database connection
+conn = create_connection("/home/jopi/Desktop/cs50/final_project/local.db")
