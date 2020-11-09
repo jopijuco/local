c9517a96b113185e9e186e3475ed42d6fde1c917
jopijuco <jpintojcoelho@gmail.com>
2020-11-09 01:52:55 +0000

    add index and login pages with routing

--- application.py
+++ application.py
@@ -1,7 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return("Hello world")
+    return render_template("index.html")
+
+
+@app.route("/register")
+def register():
+    return "TODO"
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
