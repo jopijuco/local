29b144acca8722212abfda8af5f800aca9b94b83
jopijuco <jpintojcoelho@gmail.com>
2020-11-17 00:47:50 +0000

    new db file for fev; new py files that help a logical distribution

---
+++ constants.py
@@ -0,0 +1,17 @@
+# Model
+DB = "local.db"
+DEV_DB = "local_dev.db"
+
+# Requests
+POST = "POST"
+GET = "GET"
+
+# Pages
+INDEX_PAGE = "index.html"
+LOGIN_PAGE = "login.html"
+REGISTER_PAGE = "register.html"
+USER_PAGE = "user.html"
+
+# Redirect
+LOGIN = "login"
+USER = "user"
