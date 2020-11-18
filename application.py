5e567a5f59acd48588c5169c1e0508de00c0a6fe
Jade Dupont <dupont.jade@gmail.com>
2020-11-18 19:19:24 +0100

    add 1 store with a picture

--- application.py
+++ application.py
@@ -20,6 +20,7 @@ def after_request(response):
 app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
+app.config["IMAGE_UPLOADS"] = "static"
 Session(app)
 
 # Start App
