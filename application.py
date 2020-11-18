5e4614d81405347135d9b2491023f84923a718ba
jopijuco <jpintojcoelho@gmail.com>
2020-11-18 23:44:21 +0000

    fix db connection

--- application.py
+++ application.py
@@ -25,7 +25,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Connect to database
-db = SQL.Connect("sqlite:///database/{DB_DEV}")
+db = SQL(f"sqlite:///database/{DEV_DB}")
 
 # Start App
 import controller
