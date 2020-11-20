858fac151f0237cdf7418415830dcc3949db05f3
Jade <dupont.jade@gmail.com>
2020-11-20 11:51:29 +0100

    add model product

---
+++ model/product.py
@@ -0,0 +1,8 @@
+class Product:
+    def __init__(self, id, name, description, price, discount, total):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.price = price
+        self.discount = discount
+        self.total = total
