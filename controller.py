d0664c0b50e1ffe6ee4826ad39d688672ae5a0ea
Jade <dupont.jade@gmail.com>
2020-11-20 17:15:43 +0100

    add new session variable :  session["business_id"]

--- controller.py
+++ controller.py
@@ -49,7 +49,7 @@ def login():
     if request.method == POST:
         session.clear()
         try:    
-            user = db.execute("SELECT * FROM user_businesses WHERE username = :username",
+            user = db.execute("SELECT u.*, b.id as business_id FROM user_businesses u LEFT JOIN business b ON (u.id=b.user_id) WHERE username = :username",
                                 username=request.form.get("username"))
         except Error as e:
             return e.args
@@ -59,7 +59,8 @@ def login():
         if len(user) != 1:
             return "FAILED LOGIN"
 
-        session["user_id"] = user[0]["id"]      
+        session["user_id"] = user[0]["id"] 
+        session["business_id"] = user[0]["business_id"] 
         return redirect(url_for("area", username=user[0]["username"]))
 
     return render_template(LOGIN_PAGE)
@@ -81,12 +82,12 @@ def store():
             mobile = request.form.get("mobile")
             phone = request.form.get("phone")
             fiscal_number = request.form.get("fiscal_number")
-            db.execute("UPDATE business SET name=:name, fiscal_number=:fiscal_number, description=:description , mobile=:mobile , phone=:phone  WHERE id= :id", name=name, description=description, fiscal_number=fiscal_number, mobile=mobile, phone=phone, id=session["user_id"])
+            db.execute("UPDATE business SET name=:name, fiscal_number=:fiscal_number, description=:description , mobile=:mobile , phone=:phone  WHERE id= :id", name=name, description=description, fiscal_number=fiscal_number, mobile=mobile, phone=phone, id=session["business_id"])
         elif request.form['submit_button'] == 'add_store':
             for row in db.execute("SELECT MAX(id) as max_id FROM addresses"):
                 new_address_id = row["max_id"]+1
             db.execute("INSERT INTO addresses (id, street, number, zip_code, city, region, country) VALUES (:id, '', '', '', '', '', '')", id=new_address_id)
-            db.execute("INSERT INTO stores (business_id, address_id)  VALUES (:id, :address_id)", id=session["user_id"], address_id=new_address_id)
+            db.execute("INSERT INTO stores (business_id, address_id)  VALUES (:id, :address_id)", id=session["business_id"], address_id=new_address_id)
         else:
             store_id = request.form['submit_button']
             #store's image update
@@ -104,15 +105,15 @@ def store():
             country = request.form.get("country_"+store_id)
             db.execute("UPDATE addresses SET number=:number, street=:street, zip_code=:zip_code, city=:city, region=:region, country=:country WHERE id= (SELECT address_id FROM stores WHERE id=:id)", number = number, street = street, zip_code = zip_code, city = city, region = region, country = country, id=store_id)
 
-    business = Business(session["user_id"], '', '', '', '', '')
-    for row in db.execute("SELECT * FROM business WHERE id = :id", id=session["user_id"]):
+    business = Business(session["business_id"], '', '', '', '', '')
+    for row in db.execute("SELECT * FROM business WHERE id = :id", id=session["business_id"]):
         business.name = row["name"]
         business.description = row["description"]
         business.fiscal_number = row["fiscal_number"]
         business.phone = row["phone"]
         business.mobile = row["mobile"]
     
-    for row in db.execute("SELECT s.*, a.number, a.street, a.zip_code, a.city, a.region, a.country FROM stores s LEFT JOIN addresses a ON (a.id = s.address_id) WHERE business_id = :id", id=session["user_id"]):
+    for row in db.execute("SELECT s.*, a.number, a.street, a.zip_code, a.city, a.region, a.country FROM stores s LEFT JOIN addresses a ON (a.id = s.address_id) WHERE business_id = :id", id=session["business_id"]):
         if (row["front_pic"] is None or row["front_pic"] == ""):
             front_pic = "noimgavailable.jpg"
         else:
@@ -134,7 +135,16 @@ def product():
 @login_required
 def single_product(product_id):
     if request.method == POST:
-        return "TODO"
+        name = request.form.get("name")
+        description = request.form.get("description")
+        price = request.form.get("price")
+        discount = ""
+        total = ""
+        tag_id = ""
+        img_id = ""
+        business_id = request.form.get("name")
+        if request.form['submit'] == 'add':
+            db.execute("INSERT INTO products(name, description,price,discount,total,tag_id,img_id,business_id) VALUES (:name, :description, :price, :discount, :total, :tag_id, :img_id, :business_id", name=name, description=description, price=price, discount=discount, total=total, tag_id=tag_id, img_id=img_id, business_id=business_id)
     product = Product(product_id, '', '', '', '', '')
     #if product_id == 'new':
         #product.description = ""
