898e6f6691b86f4fe3188aa2d5656cf13c860cea
Jade <dupont.jade@gmail.com>
2020-12-08 15:57:43 +0100

    history page

--- controller.py
+++ controller.py
@@ -373,7 +373,7 @@ def order():
         for row in db.execute("SELECT o.id, o.date, o.amount, o.status_id, sta.name AS status_name, sto.name AS store_name, c.first_name || ' ' || c.last_name AS customer_name FROM orders o INNER JOIN status sta ON (o.status_id = sta.id) INNER JOIN stores sto ON (o.store_id = sto.id) INNER JOIN customers c ON (c.id = o.customer_id) WHERE store_id IN (select id FROM stores WHERE business_id = :id) and status_id != 4", id = session["business_id"]):
             order = Order(row["id"], row["date"], row["amount"], row["status_name"], row["status_id"], row["store_name"], row["customer_name"])
             orders.append(order)
-        return render_template(ORDER_PAGE, orders=orders)
+        return render_template(ORDER_PAGE, orders=orders, history = False)
 
     if session["type"] == CUSTOMER:
         if request.method == POST:
@@ -405,14 +405,20 @@ def order_details(order_id):
             order = Order(row["id"], row["date"], row["amount"], row["status_name"], row["status_id"],row["store_name"], row["customer_name"])
         return render_template(ORDER_DETAILS_PAGE, order = order, status_list = status_list)
 
-@app.route("/history", methods=[GET, POST])
+
+@app.route("/history")
 @login_required
+#to do : customer view
 def history():
-    if request.method == POST:
-        return "TODO"
-    return render_template(HISTORY_PAGE)
+    if session["type"] == BUSINESS:
+        orders = []
+        #retrieve all completed orders (status_id = 4)
+        for row in db.execute("SELECT o.id, o.date, o.amount, o.status_id, sta.name AS status_name, sto.name AS store_name, c.first_name || ' ' || c.last_name AS customer_name FROM orders o INNER JOIN status sta ON (o.status_id = sta.id) INNER JOIN stores sto ON (o.store_id = sto.id) INNER JOIN customers c ON (c.id = o.customer_id) WHERE store_id IN (select id FROM stores WHERE business_id = :id) and status_id = 4", id = session["business_id"]):
+            order = Order(row["id"], row["date"], row["amount"], row["status_name"], row["status_id"], row["store_name"], row["customer_name"])
+            orders.append(order)
+        return render_template(ORDER_PAGE, orders=orders, history = True)
 
-
+    
 @app.route("/account", methods=[GET, POST])
 @login_required
 def account():  
