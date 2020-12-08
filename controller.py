238d37a24320f10ab7a506392ac56d8b2c853294
Jade <dupont.jade@gmail.com>
2020-12-08 15:27:03 +0100

    display list of status from the DB

--- controller.py
+++ controller.py
@@ -18,6 +18,7 @@ from model.store import *
 from model.product import *
 from model.picture import *
 from model.order import *
+from model.status import *
 
 import ast
 import os
@@ -393,11 +394,15 @@ def order():
 def order_details(order_id):
     #to do : customer view
     if session["type"] == BUSINESS:
+        status_list = []
+        for row in db.execute("SELECT * FROM status"):
+            status = Status(row["id"], row["name"], row["description"])
+            status_list.append(status)
         if request.method == POST:
             return "TODO"
         for row in db.execute("SELECT o.id, o.date, o.amount, sta.name AS status_name, sto.name AS store_name, c.first_name || ' ' || c.last_name AS customer_name FROM orders o INNER JOIN status sta ON (o.status_id = sta.id) INNER JOIN stores sto ON (o.store_id = sto.id) INNER JOIN customers c ON (c.id = o.customer_id) WHERE store_id IN (select id FROM stores WHERE business_id = :id) and status_id != 4", id = session["business_id"]):
             order = Order(row["id"], row["date"], row["amount"], row["status_name"], row["store_name"], row["customer_name"])
-        return render_template(ORDER_DETAILS_PAGE, order = order)
+        return render_template(ORDER_DETAILS_PAGE, order = order, status_list = status_list)
 
 @app.route("/history", methods=[GET, POST])
 @login_required
