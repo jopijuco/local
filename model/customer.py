cc529dae5154df6d7bda268b2c2ace2930256c14
Jade <dupont.jade@gmail.com>
2020-12-08 18:04:40 +0100

    new class customer

---
+++ model/customer.py
@@ -0,0 +1,11 @@
+class Customer:
+    def __init__(self, id, name, front_pic, number, street, zip_code, city, region, country):
+        self.id = id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.number = number
+        self.street = street
+        self.zip_code = zip_code
+        self.city = city
+        self.region = region
+        self.country = country
